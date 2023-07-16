c4b3b972890ef9167732eb3cc77ede57f9540a96
Svin07 <svin07@ukr.net>
2023-07-16 20:08:49 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,24 +5,22 @@ const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
 
-player.ready().then(() => {
-    const hendlyTimeUpdate = throttle(() => {
-        const currentTime = player.getCurrentTime();
-        sessionStorage.setItem("videoplayer-curent-time", currentTime);
+
+    const hendlyTimeUpdate = throttle(({seconds}) => {
+        
+        localStorage.setItem("videoplayer-curent-time", currentTime);
     }, 1000);
     player.on("timeupdate", hendlyTimeUpdate);
 
-const savedTime = sessionStorage.getItem("videoplayer-curent-time");
-if (savedTime) { const currentTime = parseFloat(savedTime);
-    player.getDuration().then((duration) => {
-        if (currentTime >= 0 && currentTime < duration) {
-            player.setCurrentTime(currentTime);
-        } else {
-            sessionStorage.removeItem("videoplayer-curent-time");
-        }
-    })
+const savedTime = localStorage.getItem("videoplayer-curent-time");
+if (savedTime) {
     
-}
-});
+            player.setCurrentTime(savedTime);
+        
+};
+    
+    
+
+
 
 
