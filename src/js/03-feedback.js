30c2a4253714d46fcca71bf0befe6269277c17ef
Svin07 <svin07@ukr.net>
2023-07-19 21:39:45 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,15 @@
 import throttle from "lodash.throttle";
 
 const refs = {
-
-
     input: document.querySelector(".feedback-form input"),
     textarea: document.querySelector(".feedback-form textarea"),
-   
-    
-    };
+   };
 
 const form = document.querySelector(".feedback-form");
 
-form.addEventListener("input", throttle(pushFormData),500);
+form.addEventListener("input", throttle(pushFormData,500));
 
 form.addEventListener("submit", onFormSubmit);
-
-
-// console.log()
-
-
 
 let formData = {email:"", message:""};
 
@@ -27,35 +18,21 @@ function pushFormData(event) {
     formData[event.target.name] = event.target.value;
 
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-
-      
 }
-
-
-
 const localData = JSON.parse(localStorage.getItem("feedback-form-state"));
 populatteTextArea();
 
-
 function populatteTextArea() {
-
-    
 
     if(localData.email && localData.message){
         refs.input.value = localData.email;
     refs.textarea.value = localData.message;
     formData.email = localData.email;
     formData.message = localData.message;
-        // console.log(localData);
-        
-        }
+      }
 else
         {console.log("є пусті поля");}
-
-
-   
 }
-
 function onFormSubmit(evt) {
 
     evt.preventDefault();
